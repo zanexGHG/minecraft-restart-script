2178785fb43f0a573aa392e1328235ae5901038f
zanex <81015632+zanex78@users.noreply.github.com>
2023-05-04 08:47:00 +0200

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
-
 BINDIR=$(dirname "$(readlink -fn "$0")")
 cd "$BINDIR"
-echo "\033]0;test\007"
-
-
 while true
 do
- 
     java -Xmx8G -jar server.jar
-
-
-    echo 'Press STRG + C to close the script!'
-    echo "Rebooting in:"
+    echo 'Press CTRL + C to stop the restart process!'
+    echo "Restarting in:"
     for i in 3 2 1
     do
         echo "$i..."
         sleep 1
     done
-    echo 'Server rebooting!'
+    echo 'The server is now restarting!'
 done
 
